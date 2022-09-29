99220ddabe41958e1ff86c4fea6bff98cf8b5b4a
ElEnruPa <enrumontes@gmail.com>
2022-09-29 19:40:36 -0300

    Primer Push

---
+++ Usuario/Usuario.cs
@@ -0,0 +1,16 @@
+﻿namespace BiblioDeClases
+{
+    public class Usuario
+    {
+        protected string email;
+        protected string contraseña;
+        protected int id;
+
+        public Usuario (string mail, string contraseña, int id)
+        {
+            this.email = mail;
+            this.contraseña = contraseña;
+            this.id = id;
+        }
+    }
+}
