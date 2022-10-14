f329984e98d8f33e7648db7e942461f0d0affffd
ElEnruPa <enrumontes@gmail.com>
2022-10-14 00:55:50 -0300

    Cambio

--- TP1_Labo_2/Inicio.cs
+++ TP1_Labo_2/Inicio.cs
@@ -1,8 +1,8 @@
 namespace TP1_Labo_2
 {
-    public partial class Form1 : Form
+    public partial class Inicio : Form
     {
-        public Form1()
+        public Inicio()
         {
             InitializeComponent();
         }
